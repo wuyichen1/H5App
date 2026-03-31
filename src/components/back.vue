925e194e1b22423c4765b9c5feafdf93f621c964
wyc <57949470+wuyichen1@users.noreply.github.com>
2026-03-31 11:06:10 +0800

    top

--- src/components/back.vue
+++ src/components/back.vue
@@ -21,8 +21,8 @@ function handleBack() {
 <style scoped>
 .outer-box {
   width: calc(100vw * 40 / 375);
-  height: calc(100vw * 40 / 375); /* 使用宽度比例即可，不用 vh，保证圆形或正方形 */
-  border-radius: calc(100vw * 14 / 375);
+  height: calc(100vw * 40 / 375);
+  border-radius: 50%;
   background: rgba(255, 255, 255, 1);
 
   display: flex;
--- src/components/more.vue
+++ src/components/more.vue
@@ -14,7 +14,8 @@ import moreImage from '../assets/more.png'
 .outer-box {
   width: calc(100vw * 40 / 375);
   height: calc(100vw * 40 / 375); /* 使用宽度比例即可，不用 vh，保证圆形或正方形 */
-  border-radius: calc(100vw * 14 / 375);
+  /* border-radius: calc(100vw * 14 / 375); */
+  border-radius: 50%;
   background: rgba(255, 255, 255, 1);
 
   display: flex;
@@ -23,8 +24,8 @@ import moreImage from '../assets/more.png'
 }
 
 .inner-box {
-  width: calc(100vw * 32 / 375);
-  height: calc(100vw * 32 / 375);
+  width: calc(100vw * 28 / 375);
+  height: calc(100vw * 28 / 375);
   background-size: cover;  /* 图片铺满容器 */
   background-position: center;
   background-repeat: no-repeat;
